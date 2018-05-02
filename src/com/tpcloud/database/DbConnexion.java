11efb628f86493731594a3e6ffa483b67b9d7b11
babadjide <rony_patrick@hotmail.fr>
2018-05-02 15:10:31 +0100

    modif

--- src/com/tpcloud/database/DbConnexion.java
+++ src/com/tpcloud/database/DbConnexion.java
@@ -7,28 +7,40 @@ import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 
 public class DbConnexion {
-	public java.sql.Connection dbconnexion(){
+	
+	//String url = "jdbc:mysql://bt6gmrwbm-mysql.services.clever-cloud.com:3306/bt6gmrwbm?autoReconnect=true&ampcharacterEncoding=cp1250";
+	static final String URL = "jdbc:mysql://bt6gmrwbm-mysql.services.clever-cloud.com:3306/bt6gmrwbm";
+	static final String UTILISATEUR = "uoizcqliocvbhu53";
+	static final String MOTDEPASSE = "PIBLdIN5wiPtsEoNtak";
+	
+	public java.sql.Connection dbconnexion() throws Exception {
+		java.sql.Connection connexion = null;
 		/* Chargement du driver JDBC pour MySQL */
 		try {
-		    Class.forName( "com.mysql.cj.jdbc.Driver" );
+		    Class.forName( "com.mysql.jdbc.Driver" );		    
+		    connexion = DriverManager.getConnection( URL, UTILISATEUR, MOTDEPASSE );		    
 		} catch ( ClassNotFoundException e ) {
-		    System.out.println(e.getMessage());
+		    throw e;
+		} catch ( SQLException e ) {
+			throw e;
 		}
+		System.out.println("Connexion établie");
+		return connexion;
+		}
+}
 		
 		/* Connexion à la base de données */
-		String url = "jdbc:mysql://bt6gmrwbm-mysql.services.clever-cloud.com:3306/bt6gmrwbm?autoReconnect=true&ampcharacterEncoding=cp1250";
-		String utilisateur = "uoizcqliocvbhu53";
-		String motDePasse = "PIBLdIN5wiPtsEoNtak";
-		java.sql.Connection connexion = null;
-		try {
-			connexion = DriverManager.getConnection( url, utilisateur, motDePasse );
+
+		
+		//try {
+			
 			//connexion = DriverManager.getConnection("mysql -h bt6gmrwbm-mysql.services.clever-cloud.com -P 3306 -u uoizcqliocvbhu53 -p bt6gmrwbm");
 			/* Ici, nous placerons nos requêtes vers la BDD */
 			/* ... */
 
-		} catch ( SQLException e ) {
-			System.out.println(e.getMessage());
-		} //finally {
+		//} catch ( SQLException e ) {
+			//System.out.println(e.getMessage());
+		//} //finally {
 			//if ( connexion != null )
 				//try {
 					/* Fermeture de la connexion */
@@ -37,6 +49,6 @@ public class DbConnexion {
 					/* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
 				//}
 		//}
-		return connexion;
-	}	
-}
+		//return connexion;
+	//}	
+//}
